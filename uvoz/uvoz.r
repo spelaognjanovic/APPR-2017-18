68ac7894a7980966d0c51446d4da7a566c344e4e
Špela Ognjanović <spela.ognjanovic@student.fmf.uni-lj.si>
2017-12-07 10:02:25 +0100

    Uvažam

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -19,6 +19,21 @@ uvozi.hitrost.serve <- function() {
       Encoding(tabela[[i]]) <- "UTF-8"
     }
   }
+
+  return(tabela)
+}
+
+# Funkcija, ki uvozi zmagovalce Grand Slam turnirja
+uvozi.zmagovalce <- function() {
+  link <- "https://sl.wikipedia.org/wiki/Seznam_zmagovalcev_turnirjev_za_Grand_Slam_-_mo%C5%A1ki_posami%C4%8Dno"
+  stran <- html_session(link) %>% read_html()
+  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable']") %>% .[[1]] %>% html_table(fill = TRUE)
+    .[[1]] %>% html_table(dec = ",")
+  for (i in 1:ncol(tabela)) {
+    if (is.character(tabela[[i]])) {
+      Encoding(tabela[[i]]) <- "UTF-8"
+    }
+  }
   colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
                         "ustanovitev", "pokrajina", "regija", "odcepitev")
   tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
@@ -33,18 +48,29 @@ uvozi.hitrost.serve <- function() {
   return(tabela)
 }
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names = c("obcina", 1:4),
-                    locale = locale(encoding = "Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse = " ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
-  data <- data %>% melt(id.vars = "obcina", variable.name = "velikost.druzine",
-                        value.name = "stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- factor(data$obcina, levels = obcine)
-  return(data)
+# Funkcija, ki uvozi podatke o tenisačih (sezina, št. tekem, ime, starost)
+uvozi.tenisaci <- function() {
+  link <- "http://www.ultimatetennisstatistics.com/seasons"
+  stran <- html_session(link) %>% read_html()
+  tabela <- stran %>% html_nodes(xpath="//table[@class='table table-condensed table-hover table-striped']") %>%
+    .[[1]] %>% html_table(dec = ",")
+  for (i in 1:ncol(tabela)) {
+    if (is.character(tabela[[i]])) {
+      Encoding(tabela[[i]]) <- "UTF-8"
+    }
+  }
+  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
+                        "ustanovitev", "pokrajina", "regija", "odcepitev")
+  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
+  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
+  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
+  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
+    tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
+  }
+  for (col in c("obcina", "pokrajina", "regija")) {
+    tabela[[col]] <- factor(tabela[[col]])
+  }
+  return(tabela)
 }
 
 # Zapišimo podatke v razpredelnico obcine
